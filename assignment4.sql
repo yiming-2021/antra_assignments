14261ab1f84aeedeb83b2c49a46a84ad7b462a35
Yiming <90655469+yiming-2021@users.noreply.github.com>
2021-09-20 19:17:41 -0400

    Update assignment4.sql

--- assignment4.sql
+++ assignment4.sql
@@ -3,7 +3,7 @@
 		-- A view is a virtual table whose content is defined by a query. Using a view, we can obtain a subset of a existing table, or join multiple tables. Also, views can save space, because it only define the table instead of store the content of the table.
 	
 	--2. Can data be modified through views?
-		-- No. You can only modiify the content in the view, but cannot modify the data in the referrenced tables.
+		-- Yes. Data in the base table will be modified.
 	
 	--3. What is stored procedure and what are the benefits of using it?
 		-- A stored procedure is a query which can be saved to reuse over and over again. It provides not only the higher productivity but also increased scalabity.
@@ -39,10 +39,10 @@
 	use Northwind
 	go
 
-	select * from Region
+
 	--1. Lock tables Region, Territories, EmployeeTerritories and Employees. Insert following information into the database. In case of an error, no changes should be made to DB.
 		--a. A new region called “Middle Earth”;
-		LOCK TABLE Region IN EXCLUSIVE MODE;
+		BEGIN TRAN
 		insert Region values(5, 'Middle Earth');
 
 		--b. A new territory called “Gondor”, belongs to region “Middle Earth”;
@@ -54,10 +54,14 @@
 		select * from EmployeeTerritories
 		insert Employees(LastName, FirstName,Region) values ('Aragorn','King','Middle Earth')
 		insert EmployeeTerritories values(10,99999)
+		
 
-
+		--select * from Region
+		--select * from Territories
+		--select * from EmployeeTerritories
 
 	--2. Change territory “Gondor” to “Arnor”.
+
 		update Territories 
 		set TerritoryDescription = 'Arnor'
 		where TerritoryDescription = 'Gondor'
@@ -66,7 +70,8 @@
 	--3. Delete Region “Middle Earth”. (tip: remove referenced data first) (Caution: do not forget WHERE or you will delete everything.) In case of an error, no changes should be made to DB. Unlock the tables mentioned in question 1.
 		delete from Region 
 		where RegionDescription = 'Middle Earth';
-	
+		
+		ROLLBACK
 	
 	--4. Create a view named “view_product_order_[your_last_name]”, list all products and total ordered quantity for that product.
 		create view view_product_order_gu 
@@ -78,7 +83,7 @@
 		group by p.ProductID;
 		
 	--5. Create a stored procedure “sp_product_order_quantity_[your_last_name]” that accept product id as an input and total quantities of order as output parameter.
-		CREATE PROCEDURE sp_product_order_quantity_gu
+		CREATE PROC sp_product_order_quantity_gu
 		@Productid int
 		as
 		select totalOrdered
@@ -90,7 +95,7 @@
 
 
 	--6. Create a stored procedure “sp_product_order_city_[your_last_name]” that accept product name as an input and top 5 cities that ordered most that product combined with the total quantity of that product ordered from that city as output.
-		CREATE PROCEDURE sp_product_order_city_gu
+		CREATE PROC sp_product_order_city_gu
 		@ProductName nvarchar(40)
 		as
 		select top 5 o.ShipCity, sum(od.Quantity) totalQuantity
@@ -108,6 +113,7 @@
 
 		
 	--7. Lock tables Region, Territories, EmployeeTerritories and Employees. Create a stored procedure “sp_move_employees_[your_last_name]” that automatically find all employees in territory “Tory”; if more than 0 found, insert a new territory “Stevens Point” of region “North” to the database, and then move those employees to “Stevens Point”.
+		BEGIN TRAN
 		CREATE PROCEDURE sp_move_employees_gu
 		as
 		BEGIN
@@ -132,60 +138,62 @@
 		end
 
 
-	
+		ROLLBACK
 	
 	--8. Create a trigger that when there are more than 100 employees in territory “Stevens Point”, move them back to Troy. (After test your code,) remove the trigger. Move those employees back to “Troy”, if any. Unlock the tables.
-		create trigger triggerMove 
-		after insert 
-		on 
-		EmployeeTerritories
-		for each row
-		UPDATE EmployeeTerritories
-		SET TerritoryID = 99998
+		BEGIN TRAN
+		CREATE TRIGGER triggerMove ON EmployeeTerritories
+		FOR INSERT AS
+		BEGIN
+		   UPDATE EmployeeTerritories
+			SET TerritoryID = 99998
+		END
 
-
+		ROLLBACK
 
 	--9. Create 2 new tables “people_your_last_name” “city_your_last_name”. City table has two records: {Id:1, City: Seattle}, {Id:2, City: Green Bay}. People has three records: {id:1, Name: Aaron Rodgers, City: 2}, {id:2, Name: Russell Wilson, City:1}, {Id: 3, Name: Jody Nelson, City:2}. Remove city of Seattle. If there was anyone from Seattle, put them into a new city “Madison”. Create a view “Packers_your_name” lists all people from Green Bay. If any error occurred, no changes should be made to DB. (after test) Drop both tables and view.
-		create table ##people_gu (id int, Name nvarchar(20), City int)
-		insert ##people_gu values (1, 'Aaron Rodgers',2)
-		insert ##people_gu values (2, 'Russell Wilson',1)
-		insert ##people_gu values (3, 'Jody Nelson',2)
+		create table people_gu (id int, Name nvarchar(20), City int)
+		insert people_gu values (1, 'Aaron Rodgers',2)
+		insert people_gu values (2, 'Russell Wilson',1)
+		insert people_gu values (3, 'Jody Nelson',2)
 
-		create table ##city_gu (id int, City nvarchar(20))
-		insert ##city_gu values (1, 'Seattle')
-		insert ##city_gu values (2, 'Green Bay')
+		create table city_gu (id int, City nvarchar(20))
+		insert city_gu values (1, 'Seattle')
+		insert city_gu values (2, 'Green Bay')
 		
-		DELETE FROM ##city_gu WHERE City = 'Seattle'
-		insert ##city_gu values (3, 'Madison')
-		UPDATE ##people_gu
+		DELETE FROM city_gu WHERE City = 'Seattle'
+		insert city_gu values (3, 'Madison')
+		UPDATE people_gu
 		set city = 3
 		where city = 1
 
-		select * from ##city_gu
-		select * from ##people_gu
+		select * from city_gu
+		select * from people_gu
 
 
-		create view ##Packers_Yiming
+		create view Packers_Yiming
 		as 
 		select name 
-		from ##people_gu p,  ##city_gu c
+		from people_gu p,  city_gu c
 		where p.City = c.id and c.City = 'Green Bay'
 
-
-
+		drop table people_gu, city_gu
+		drop view Packers_Yiming
 		
 	--10.  Create a stored procedure “sp_birthday_employees_[you_last_name]” that creates a new table “birthday_employees_your_last_name” and fill it with all employees that have a birthday on Feb. (Make a screen shot) drop the table. Employee table should not be affected.
 		create procedure sp_birthday_employees_gu
 		as 
-			create table birthday_employees_your_last_name 
-			as
-			select *
-			from Employees
-			where MONTH(BirthDate) = 2
+			Select * into birthday_employees_gu  from  Employees  where MONTH(BirthDate) = 2
+		go
+
+		EXEC sp_birthday_employees_gu
+		Drop table birthday_employees_gu
+
+
 
 
 	--11. Create a stored procedure named “sp_your_last_name_1” that returns all cites that have at least 2 customers who have bought no or only one kind of product. Create a stored procedure named “sp_your_last_name_2” that returns the same but using a different approach. (sub-query and no-sub-query).
-		create procedure sp_your_gu_1
+		create proc sp_your_gu_1
 		as
 		select City
 		from Customers
